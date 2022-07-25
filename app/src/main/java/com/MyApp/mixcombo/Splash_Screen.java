8e450e30ce0dd8fce055fbc957906a3d516e6186
Hero <angadpreetsingh147@gmail.com>
2022-07-25 22:29:05 +0530

    Mix Combo App

--- app/src/main/java/com/MyApp/mixcombo/Splash_Screen.java
+++ app/src/main/java/com/MyApp/mixcombo/Splash_Screen.java
@@ -2,7 +2,12 @@ package com.MyApp.mixcombo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash_Screen extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        Intent next=new Intent(Splash_Screen.this,CalculatorScreen.class);
+        Animation anim= AnimationUtils.loadAnimation(this,R.anim.logo_animation);
+        ImageView logo=(ImageView)findViewById(R.id.logo);
+        logo.startAnimation(anim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(next);
+                finish();
+            }
+        },4000);
     }
 }
